9d2ac8b25d68020134049d4ba783ccfd6eb698f6
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2022-11-16 19:48:11 +0900

    복구

--- models/Review.js
+++ models/Review.js
@@ -9,10 +9,12 @@ const reviewSchema = mongoose.Schema({
     required : true
   },
   star : {
-    type: String
+    type: String,
+    required : true
   },
   content: {
-    type : String
+    type : String,
+    required : true
   }
 
 }, {timestamps: true})
--- routes/review.js
+++ routes/review.js
@@ -20,10 +20,9 @@ router.post("/saveReview", (req, res) => {
   const review = new Review(req.body)
 
   review.save((err, doc) => {
-    console.log(err)
-    console.log(doc)
-    if(err) return res.status(400).send(err)
-    else res.status(200).send(doc)
+    console.log(req.body)
+    if(err) return res.status(200).json({success : false, tit : req.body.title, st : req.body.star, cont : req.body.content })
+    else res.status(200).json({success : true, msg : "등록완료!"})
   })
 })
 
